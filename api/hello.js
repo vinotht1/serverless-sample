3bdff4242aee9d5db26e374bc57315fb78e35f85
Vinoth <vinoth.login93@gmail.com>
2021-03-24 15:31:36 +0530

    Axios check

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+// const axios = require('axios');
+import axios from "axios"
 module.exports = async(req, res) => {
     let postHooks = await axios.get('https://development.homestaymatch.com/get-csrf')
     res.json({status:postHooks.headers})
